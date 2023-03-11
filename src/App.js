232b9e637d7758bbe5bdf4373f85769638ab3d72
David Whipple <dwhipp88@gmail.com>
2023-03-10 17:03:11 -0700

    created hard coded html elements, mapped through comment array, brought in space image as an asset

--- src/App.js
+++ src/App.js
@@ -1,30 +1,98 @@
-import logo from './logo.svg';
 import './App.css';
 
 import { AiOutlineStar } from 'react-icons/ai';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <h3>
-          <AiOutlineStar />
-        </h3>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const directoryOfComments = [
+        {
+            id: 1,
+            author: 'John Doe',
+            comment: "I am John Doe's comment.",
+            timeStamp: '8:00AM',
+            dateStamp: '1/1/2023',
+        },
+        {
+            id: 2,
+            author: 'Jane Doe',
+            comment: "I am Jane Doe's comment.",
+            timeStamp: '9:00AM',
+            dateStamp: '22/2/2023',
+        },
+        {
+            id: 3,
+            author: 'Jimmy Doe',
+            comment: "I am Jimmy Doe's comment.",
+            timeStamp: '8:00AM',
+            dateStamp: '11/8/2023',
+        },
+        {
+            id: 4,
+            author: 'Jannette Doe',
+            comment: "I am Jannette Doe's comment.",
+            timeStamp: '8:00AM',
+            dateStamp: '2/9/2023',
+        },
+        {
+            id: 5,
+            author: 'Josh Doe',
+            comment: "I am Josh Doe's comment.",
+            timeStamp: '8:00AM',
+            dateStamp: '10/11/2023',
+        },
+    ];
+
+    return (
+        <div className="body-container">
+            <nav>
+                <h1>Star Talk Reforged</h1>
+                <ul>
+                    <li>Login</li>
+                    <li>Signup</li>
+                </ul>
+            </nav>
+            <div className="topics-container">
+                <div className="image-container" />
+                   
+                <div className="discussion-container">
+                    <h2>The Discussion</h2>
+                    {directoryOfComments.map(
+                        ({
+                            id,
+                            author,
+                            comment,
+                            timeStamp,
+                            dateStamp,
+                        }) => (
+                            <div
+                                key={id}
+                                className="comment-container"
+                            >
+                                <p className="comment-text">
+                                    {comment}
+                                </p>
+                                <p className="author-text">
+                                    Written by:{' '}
+                                    <span className="authors-name">
+                                        {author}
+                                    </span>
+                                </p>
+                                <p className="time-stamp">
+                                    on{' '}
+                                    <span className="date">
+                                        {dateStamp}
+                                    </span>{' '}
+                                    at{' '}
+                                    <span className="time">
+                                        {timeStamp}
+                                    </span>
+                                </p>
+                            </div>
+                        )
+                    )}
+                </div>
+            </div>
+        </div>
+    );
+};
 
 export default App;
